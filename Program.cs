65e1cbc1a9309e84b754e768c6cf86301ddbc7b6
Olga <omfe1811@gmail.com>
2024-12-02 12:35:36 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace firstsharp
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi world");
+        }
+    }
+}
