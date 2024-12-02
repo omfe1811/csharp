ea2d9fba25a651b6a7bbc5f16ed7bc3b572b1792
Olga <omfe1811@gmail.com>
2024-12-02 11:21:10 +0100

    dddd

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45775efd2d3ffd1d7502bb0aeb380281c0a3e2d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+781ad36b9cb98de2b8db3b833948896915599bd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ example.js
@@ -0,0 +1,32 @@
+﻿//class example with javascript
+class Person {
+  constructor(name, age, salary) {
+    this.name = name;
+    this.age = age;
+    this.salary = salary;
+  }
+
+  greeting() {
+    //void
+    console.log(
+      `hi, my name is ${this.name}  my age is ${this.age}` //my salary is ${this.salary}`
+    );
+  }
+
+  personSalary() {
+    console.log(`my salary is ${this.salary}`);
+  }
+}
+
+const person1 = new Person("Joe", 25, 40000);
+const person2 = new Person("Olga", 26, 5000000);
+const person3 = new Person("Leonora", 27, 4000000);
+
+person1.greeting();
+person2.greeting();
+person3.greeting();
+person1.personSalary();
+person2.personSalary();
+person3.personSalary();
+
+console.log(person1.salary);
